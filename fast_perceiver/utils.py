f8873c0c7602c4fcf0b1991e242b2d0013f89793
Kristian Klemon <kristian.klemon@gmail.com>
2023-09-26 12:05:53 +0000

    Implement tests

--- fast_perceiver/utils.py
+++ fast_perceiver/utils.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 from functools import wraps
 
 
@@ -14,3 +17,16 @@ def cache_fn(f):
         cache[key] = result
         return result
     return cached_fn
+
+
+def random_mask(x):
+    bs, n = x.shape[:2]
+
+    seq_lens = torch.randint(1, n + 1, (bs,), device=x.device)
+    mask = torch.arange(n, device=x.device)[None, :] < seq_lens[:, None]
+
+    return mask
+
+
+def numel(m: nn.Module, only_trainable: bool = True):
+    return sum(p.numel() for p in m.parameters() if not only_trainable or p.requires_grad)
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import torch
+
+
+torch.set_default_device('cuda')
+torch.set_default_dtype(torch.float16)
